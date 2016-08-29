08e88ba0292ee6de8f932aec4f9b6621746410e7
Arpita Tugave <Arpita Tugave>
2016-08-28 22:20:03 -0400

    version 2

--- lens.js
+++ lens.js
@@ -1,8 +1,8 @@
 	    var pi=Math.PI; //pi value
-	    var init_radius = 100;
-	    var theta_increment = 5
-	    var radius_increment = 5;
-	    var camera, controls, scene, renderer , theta , theta_prev, geom, mesh;
+	    var radius = 100;
+	    var theta_increment = 20;
+	    var phi_increment = 20;
+	    var camera, controls, scene, renderer , theta , phi, theta_prev, phi_prev, geom, mesh;
 	    var v1,v2,v3;
 
         init();
@@ -26,34 +26,36 @@
 					wireframe: true,
 					wireframeLinewidth: 40 
 					});
+				v1 = new THREE.Vector3(0,0,0);
 
 
-				for ( theta = theta_increment ; theta <= 360 ; theta = theta + theta_increment){
+				for ( phi = phi_increment ; phi <= 180 ; phi = phi + phi_increment){
+					for ( theta = theta_increment ; theta <= 360 ; theta = theta + theta_increment){
 
-				 	v1 = new THREE.Vector3(0,0,0);
-				 	radius = radius_increment;
+							geom = new THREE.Geometry();
+							x_value = radius * Math.sin(theta) * Math.cos(phi);
+							y_value = radius * Math.sin(theta) * Math.sin(phi);
+							z_value = radius * Math.cos(theta);
+							x_value_prev = radius * Math.sin(theta_prev) * Math.cos(phi_prev);
+							y_value_prev = radius * Math.sin(theta_prev) * Math.sin(phi_prev);
+							z_value_prev = radius * Math.cos(theta_prev);
+							v2 = new THREE.Vector3(x_value,y_value,z_value);
+							v3 = new THREE.Vector3(x_value_prev,y_value_prev,z_value_prev);
 
-					while(radius <= init_radius){
+							geom.vertices.push( v1 );
+							geom.vertices.push( v2 );
+							geom.vertices.push( v3 );
 
-						geom = new THREE.Geometry();
-						z_value = Math.sqrt((radius*radius)-(radius * Math.cos(theta*pi/180))-(radius * Math.sin(theta*pi/180)));
-						v2 = new THREE.Vector3(radius * Math.cos(theta*pi/180),radius * Math.sin(theta*pi/180),z_value);
-						v3 = new THREE.Vector3(radius * Math.cos(theta_prev*pi/180),radius * Math.sin(theta_prev*pi/180),z_value);
+							geom.faces.push( new THREE.Face3( 0, 1, 2 ) );
+							geom.computeFaceNormals();
 
-						geom.vertices.push( v1 );
-						geom.vertices.push( v2 );
-						geom.vertices.push( v3 );
-
-						geom.faces.push( new THREE.Face3( 0, 1, 2 ) );
-						geom.computeFaceNormals();
-
-						mesh = new THREE.Mesh( geom, material);
-                		scene.add(mesh);
-                		render();
-                		v1 = v2;
-                		radius = radius + radius_increment;
+							mesh = new THREE.Mesh( geom, material);
+	                		scene.add(mesh);
+	                		render();
+	                		v1 = v2;
+	                		theta_prev = theta;
 					}
-					theta_prev = theta;
+					phi_prev = phi;
 				}
 
 		window.addEventListener(' resize' , onWindowResize , false);
